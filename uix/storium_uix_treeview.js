0043d7377909f004faf0622facabfb065035ae45
Raj <libraryhergertalan@gmail.com>
2025-09-29 02:17:38 +0000

    restructuring

---
+++ uix/storium_uix_treeview.js
@@ -0,0 +1,9 @@
+// TODO: UIX/DATA FILES
+// - uix/storium_uix.js: Main UI rendering logic
+// - uix/storium_uix_treeview.js: TreeView rendering logic (modularize from main UIX)
+// - uix/storium_uix_eventfactory.js: Event wiring/factory for UI events
+// - data/storium_data.js: Data model, CRUD, and RelationalDb integration
+// - data/storium_data_treebuilder.js: Tree data builder logic (modularize from main data)
+// - data/storium_data_textarea_table_builder.js: Textarea table loader/parser logic
+//
+// TODO: Modularize treeview and event logic into the new files above.
